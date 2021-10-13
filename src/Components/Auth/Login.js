c3522dfa8edd455a2158d48458f6cd1d202000e2
Alexis Evaristo López Gómez <ilegal_sprite@outlook.com>
2021-10-12 20:18:34 -0500

    we build the Login component

--- src/Components/Auth/Login.js
+++ src/Components/Auth/Login.js
@@ -1,9 +1,46 @@
 import React from 'react'
 
 export const Login = () => {
+
+    const handleOnChange = () => {}; 
+
     return (
-        <h1>
-           Desde Login 
-        </h1>
+        <div className='form-usuario'>
+            <div className='contenedor-form sombra-dark'>
+                <h1>Iniciar Sesión</h1>
+
+                <form>
+                    <div className='campo-form'>
+                        <label htmlFor='email'>Email: </label>
+                        <input 
+                            type='email'
+                            id='email'
+                            name='email'
+                            placeholder='Tu Email'
+                            onChange={ handleOnChange }
+                        />
+                    </div>
+                    
+                    <div className='campo-form'>
+                        <label htmlFor='password'>Password: </label>
+                        <input 
+                            type='password'
+                            id='password'
+                            name='password'
+                            placeholder='Tu Password'
+                            onChange={ handleOnChange }
+                        />
+                    </div>
+
+                    <div className='campo-form'>
+                        <input 
+                            type='submit'
+                            className='btn btn-primario btn-block'
+                            value='Iniciar Sesión'
+                        />
+                    </div>
+                </form>
+            </div>
+        </div>
     )
 }
