0a478b37bc7820471f6d2ae7e8f0268d2ee2a4be
Alexis Evaristo López Gómez <ilegal_sprite@outlook.com>
2021-10-13 22:19:53 -0500

    finish the first part of the NewProject component

--- src/Components/Projects/NuevoProyecto.js
+++ src/Components/Projects/NuevoProyecto.js
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const NuevoProyecto = () => {
+
+    // * State para Proyecto
+    const [ proyecto, setProyecto ] = useState({
+        nombre: ''
+    });
+
+    const { nombre } = proyecto;
+
+    // TODO: Lee los contenidos del input
+    const handleOnChange = ( e ) => {
+        setProyecto({
+            ...proyecto,
+            [e.target.name]: e.target.value
+        })
+    } 
+
+    // TODO: Cuando el usuario envia un proyecto
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        // Validar el proyecto
+
+        // agregar el state
+
+        // Reiniciar el form
+    }
+
     return (
-        <div>
-            
-        </div>
+        <>
+            <button 
+                type='button'
+                className='btn btn-block btn-primario'
+                onSubmit={ handleSubmit }
+            >
+                Nuevo Proyecto
+            </button>
+
+            <form 
+                className="formulario-nuevo-proyecto"
+            >
+                <input 
+                    type="text" 
+                    className="input-text" 
+                    placeholder='Nombre Proyecto'
+                    name='nombre'
+                    value={ nombre }
+                    onClick={ handleOnChange }
+                />
+
+                <input 
+                    type="submit" 
+                    className='btn btn-primario btn-block' 
+                    value="Agregar Proyecto" 
+                />
+            </form>
+        </>
     )
 }
--- src/Components/layout/Sidebar.js
+++ src/Components/layout/Sidebar.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { NuevoProyecto } from '../Projects/NuevoProyecto';
 
 export const Sidebar = () => {
     return (
         <aside>
             <h1>MERN<span>Task</span></h1>
 
+            <NuevoProyecto />
+            
             <div className="proyectos">
                 <h2>Tus Proyectos</h2>
             </div>
