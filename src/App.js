d84f2a7bcabf8253cb79f385ae0e248a2a9472be
Alexis Evaristo López Gómez <ilegal_sprite@outlook.com>
2021-10-21 20:40:40 -0500

    we create the state of the tasks

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Login } from './Components/Auth/Login';
 import { NuevaCuenta } from './Components/Auth/NuevaCuenta';
 import { Proyectos } from './Components/Projects/Proyectos';
+import { AlertaState } from './Context/alerta/alertaState';
 
 import { ProyectoState } from './Context/Projects/ProyectoState';
 import { TareaState } from './Context/tasks/tareaState';
@@ -12,13 +13,15 @@ function App() {
   return (
     <ProyectoState>
       <TareaState>
-        <BrowserRouter>
-            <Switch>
-              <Route exact path="/" component={ Login } />
-              <Route exact path="/nueva-cuenta" component={ NuevaCuenta } />
-              <Route exact path="/proyectos" component={ Proyectos } />
-            </Switch>
-        </BrowserRouter>
+        <AlertaState>
+          <BrowserRouter>
+              <Switch>
+                <Route exact path="/" component={ Login } />
+                <Route exact path="/nueva-cuenta" component={ NuevaCuenta } />
+                <Route exact path="/proyectos" component={ Proyectos } />
+              </Switch>
+          </BrowserRouter>
+        </AlertaState>
       </TareaState>
     </ProyectoState>
   );
